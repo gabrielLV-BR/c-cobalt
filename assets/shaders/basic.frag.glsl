2ae3642ebe8b6a838da2828e58e29de2fae267bc
Gabriel L. <gabriellovato15@gmail.com>
2023-12-05 12:27:44 -0300

    Shader changes for easier visualization

--- assets/shaders/basic.frag.glsl
+++ assets/shaders/basic.frag.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
-in vec2 aUV;
 out vec4 outColor;
 
 uniform sampler2D uDiffuseTexture;
 
 void main() {
-    outColor = texture(uDiffuseTexture, aUV);
+    outColor = texture(uDiffuseTexture, vec2(0.0, 0.0));
+    outColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- assets/shaders/basic.vert.glsl
+++ assets/shaders/basic.vert.glsl
@@ -1,15 +1,13 @@
 #version 330 core
 
 layout(location = 0) in vec3 inPosition;
-layout(location = 1) in vec2 inUV;
+layout(location = 1) in vec3 inNormal;
+layout(location = 2) in vec2 inUV;
 
 uniform mat4 uModel;
 uniform mat4 uView;
 uniform mat4 uProjection;
 
-out vec2 aUV;
-
 void main() {
     gl_Position = uProjection * uView * uModel * vec4(inPosition, 1.0);
-    aUV = inUV;
 }
